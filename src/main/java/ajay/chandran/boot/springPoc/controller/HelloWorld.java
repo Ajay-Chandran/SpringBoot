f963979a3da5dc27fd94a2dfc4a50e188d9e9fe0
ajaychandran <ajaychandran43@gmail.com>
2021-08-08 17:03:53 +0200

    Added parameter to the service

--- src/main/java/ajay/chandran/boot/springPoc/controller/HelloWorld.java
+++ src/main/java/ajay/chandran/boot/springPoc/controller/HelloWorld.java
@@ -1,13 +1,26 @@
 package ajay.chandran.boot.springPoc.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 
 public class HelloWorld {
     @RequestMapping("/")
-    public String index(){
+    public String helloWorld(){
         return "Hello Spring world";
+    }
+    @RequestMapping("/ajay")
+    public String helloAjay(){
+        return "Hello Spring Ajay";
+    }
+    @GetMapping("/hello")
+    public String helloName(@RequestParam(name="name", required = false, defaultValue = "Its me") String name)
+    {
+        return "Hello " + name;
+    }
+    @GetMapping("/helloName/{name}")
+    public String hello0(@PathVariable("name") String name)
+    {
+        return "Hello " + name;
     }
 }
