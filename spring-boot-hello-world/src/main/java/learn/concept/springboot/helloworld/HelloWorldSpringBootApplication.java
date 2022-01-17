445aa04e452c34ba8329a5548c4ccef5aba74716
ajaychandran <ajaychandran43@gmail.com>
2022-01-17 22:55:40 +0200

    added custom Banner

--- spring-boot-hello-world/src/main/java/learn/concept/springboot/helloworld/HelloWorldSpringBootApplication.java
+++ spring-boot-hello-world/src/main/java/learn/concept/springboot/helloworld/HelloWorldSpringBootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+		//SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+		SpringApplication app = new SpringApplication(HelloWorldSpringBootApplication.class);
+		app.run(args);
 	}
 
 }
